bd4f622b020b695ec2244477dd64f753bb4a9fd1
Mason Schneider <schneimd@rose-hulman.edu>
2013-12-10 19:30:53 -0500

    Tests for summoner by name

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from info import API_KEY
 def main():
     print "Let's get started!"
     lolpy = LoLpy.LoLpy(API_KEY)
-    print lolpy.get_summoner_by_name('Siren Swag')
+    print lolpy.get_summoner_by_name('Anim0site')
 
 
 if __name__ == "__main__":
