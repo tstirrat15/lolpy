d0cbcb246a408ce7c0247c99bee747bd7fc5a24e
Mason Schneider <schneimd@rose-hulman.edu>
2013-12-10 19:49:31 -0500

    Small QA

--- example.py
+++ example.py
@@ -4,7 +4,10 @@ from info import API_KEY
 def main():
     print "Let's get started!"
     lolpy = LoLpy.LoLpy(API_KEY)
-    print lolpy.get_summoner_by_name('Anim0site')
+    account = lolpy.get_summoner_by_name('Dyrus')
+    league = lolpy.get_league_by_id(account['id'])
+    ranked = lolpy.get_stats_ranked('Dyrus')
+    print ranked
 
 
 if __name__ == "__main__":
